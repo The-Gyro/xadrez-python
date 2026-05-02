87a1e75658eb5041b2992d87e3f47e15fffe92e4
Gyro <hariel_yg@outlook.com>
2026-05-02 10:12:41 -0300

    frontend integrado ao flask

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, request, jsonify
+from flask import render_template
 from flask_cors import CORS
 import main
 import os
@@ -30,3 +31,7 @@ def reset():
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
     app.run(host="0.0.0.0", port=port)
+
+@app.route("/")
+def home():
+    return render_template("index.html")
