0c0a576558e9d03d3dc8a95cacc3bc10f753f919
Gyro <hariel_yg@outlook.com>
2026-05-02 09:01:39 -0300

    preparando deploy

--- app.py
+++ app.py
@@ -17,4 +17,7 @@ def move():
 
     return jsonify(main.processar_jogada(origem, destino))
 
-app.run(debug=True)
+import os
+
+port = int(os.environ.get("PORT", 5000))
+app.run(host="0.0.0.0", port=port)
